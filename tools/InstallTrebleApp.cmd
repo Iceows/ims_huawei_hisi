2eeeb79d2988a3400e1845825dd54ff2ac2c2662
Iceows <mounierr07@gmail.com>
2025-01-16 13:36:35 +0100

    TrebleApp - update with 2025 release

--- tools/InstallTrebleApp.cmd
+++ tools/InstallTrebleApp.cmd
@@ -1,9 +1,13 @@
-xcopy /Y "C:\GitRepo\iceows\treble_app\app\build\outputs\apk\release\app-release-unsigned.apk" .
-java.exe -jar "ApkSigner.jar" sign  --key platform.pk8 --cert platform.x509.pem  --v4-signing-enabled false --out "TrebleApp.apk" "app-release-unsigned.apk"
+xcopy /Y "C:\GitHub\iceows\treble_app_iceows\app\build\outputs\apk\debug\app-debug.apk" .
+java.exe -jar "ApkSigner.jar" sign  --key platform.pk8 --cert platform.x509.pem  --v4-signing-enabled false --out "TrebleApp.apk" "app-debug.apk"
 
 adb root
 adb remount rw, /system
 adb push TrebleApp.apk /system/priv-app/TrebleApp
 adb shell chmod 755 /system/priv-app/TrebleApp/TrebleApp.apk 
+
+adb push treble-overlay-telephony-hw-ims.apk /system/product/overlay/treble-overlay-telephony-hw-ims.apk
+adb shell chmod 644 /system/product/overlay/treble-overlay-telephony-hw-ims.apk
+
 
 adb reboot
