600561eb3aeb50ed200c7e483b86e4fa4792ddec
Iceows <mounierr07@gmail.com>
2025-01-19 11:01:58 +0100

    TrebleApp - Fix remount system error

--- tools/InstallTrebleApp.cmd
+++ tools/InstallTrebleApp.cmd
@@ -2,12 +2,12 @@ xcopy /Y "C:\GitHub\iceows\treble_app_iceows\app\build\outputs\apk\debug\app-deb
 java.exe -jar "ApkSigner.jar" sign  --key platform.pk8 --cert platform.x509.pem  --v4-signing-enabled false --out "TrebleApp.apk" "app-debug.apk"
 
 adb root
-adb remount rw, /system
+adb remount /system
 adb push TrebleApp.apk /system/priv-app/TrebleApp
 adb shell chmod 755 /system/priv-app/TrebleApp/TrebleApp.apk 
 
-adb push treble-overlay-telephony-hw-ims.apk /system/product/overlay/treble-overlay-telephony-hw-ims.apk
-adb shell chmod 644 /system/product/overlay/treble-overlay-telephony-hw-ims.apk
+rem adb push treble-overlay-telephony-hw-ims.apk /system/product/overlay/treble-overlay-telephony-hw-ims.apk
+rem adb shell chmod 644 /system/product/overlay/treble-overlay-telephony-hw-ims.apk
 
 
 adb reboot
