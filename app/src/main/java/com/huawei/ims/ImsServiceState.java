58484de1a6e2cae049a7eec3d1c50e0d427a37eb
Iceows <mounierr07@gmail.com>
2025-01-23 09:01:33 +0100

    Add comments in ImsServiceState classe

--- app/src/main/java/com/huawei/ims/ImsServiceState.java
+++ app/src/main/java/com/huawei/ims/ImsServiceState.java
@@ -1,8 +1,33 @@
+/*
+ * This file is part of HwIms
+ * Copyright (C) 2025 Penn Mackintosh and Raphael Mounier
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+      ImsServiceState class was not part of android repos before Android 9 version (los16)
+      I found it in the huawei ims but especially also in the msm8996 ims
+      https://github.com/bcyj/android_tools_leeco_msm8996/blob/master/telephony-apps/ims/src/org/codeaurora/ims/CallServiceState.java
+
+      It seems that this is part of a set of classes published by codeaurora
+ */
+
 package com.huawei.ims;
 
 import android.os.Parcel;
 
-/* loaded from: ImsServiceState.class */
+
 public class ImsServiceState {
     public static final int CALL_TYPE_CS_VS_RX = 6;
     public static final int CALL_TYPE_CS_VS_TX = 5;
